bd57162c44c826380043c6c32a47e9740cc9d088
jaqubm <jaqobgm@gmail.com>
2024-10-11 18:23:35 +0200

    Added Api Status endpoint

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+using calorieCounter_backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calorieCounter_backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ApiController(IConfiguration config) : ControllerBase
+{
+    private readonly DataContext _entityFramework = new(config);
+
+    [HttpGet("Status")]
+    public async Task<ActionResult<Dictionary<string, string>>> GetStatus()
+    {
+        var databaseConnectionStatus = await _entityFramework.Database.CanConnectAsync();
+
+        return Ok(new Dictionary<string, string>
+        {
+            { "apiStatus", "OK" },
+            { "databaseConnectionStatus", databaseConnectionStatus ? "OK" : "Failed" }
+        });
+    }
+}
