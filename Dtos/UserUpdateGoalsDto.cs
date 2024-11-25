e25f00140d9f987945eae7cb5cc37f893964f0b5
jaqubm <jaqobgm@gmail.com>
2024-11-25 18:13:35 +0100

    Updating User Goals

--- Dtos/UserUpdateGoalsDto.cs
+++ Dtos/UserUpdateGoalsDto.cs
@@ -1,9 +1,7 @@
 namespace calorieCounter_backend.Dtos;
 
-public class UserDto
+public class UserUpdateGoalsDto
 {
-    public string Email { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
     public float Energy  { get; set; }
     public float Protein { get; set; }
     public float Carbohydrates { get; set; }
