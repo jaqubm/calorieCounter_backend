252775ec2613ff5aa18c6671b93dd093a1e60470
PiotrJurek <piotrjurek9@gmail.com>
2024-12-01 13:04:19 +0100

    Fix to get and add

--- Dtos/UserEntryCreatorDto.cs
+++ Dtos/UserEntryCreatorDto.cs
@@ -3,8 +3,7 @@
 public class UserEntryCreatorDto
 {
     public string EntryType { get; set; } = string.Empty;
-    public string? ProductId { get; set; }
-    public string? RecipeId { get; set; }
+    public string Id { get; set; } = string.Empty;
     public DateTime Date { get; set; }
     public string MealType { get; set; } = string.Empty;
     public float? Weight { get; set; }
